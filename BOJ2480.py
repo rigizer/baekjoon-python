b1fba67b7112858a5b87dbc392b10581c9a0d7ec
rigizer <68414303+rigizer@users.noreply.github.com>
2022-04-07 21:23:30 +0900

    BOJ2480 문제풀이

---
+++ BOJ2480.py
@@ -0,0 +1,20 @@
+# https://www.acmicpc.net/problem/2480
+
+def calc(a, b, c):
+    if a == b and b == c:
+        return 10000 + a * 1000
+    if a == b:
+        return 1000 + a * 100
+    if b == c:
+        return 1000 + b * 100
+    if a == c:
+        return 1000 + c * 100
+    if a > b and a > c:
+        return a * 100
+    if b > a and b > c:
+        return b * 100
+    if c > a and c > b:
+        return c * 100
+
+a, b, c = input().split()
+print(calc(int(a), int(b), int(c)))
