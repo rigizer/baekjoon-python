650135acd457f94b32715ee7c3022bf2f46ff789
rigizer <68414303+rigizer@users.noreply.github.com>
2022-05-25 21:50:18 +0900

    BOJ14182 문제풀이

---
+++ BOJ14182.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/14182
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    if n <= 1000000:
+        print(n)
+    elif 1000000 < n <= 5000000:
+        print(int(n*0.9))
+    else:
+        print(int(n*0.8))
