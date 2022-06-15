e31c4dbbaf076fb914f1b55c5f49171602c62587
rigizer <68414303+rigizer@users.noreply.github.com>
2022-06-15 19:05:05 +0900

    BOJ9950 문제풀이

---
+++ BOJ9950.py
@@ -0,0 +1,13 @@
+# https://www.acmicpc.net/problem/9950
+
+while True:
+    l, w, a = map(int, input().split())
+    if l == w == a == 0:
+        break
+    if l == 0:
+        l = a // w
+    elif w == 0:
+        w = a // l
+    elif a == 0:
+        a = l * w
+    print(l, w, a)
