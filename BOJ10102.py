8c181614dcf875e72aa9d7a190433069b3eeeb6c
rigizer <rigizer@gmail.com>
2022-07-30 23:04:09 +0900

    BOJ10102 문제풀이

---
+++ BOJ10102.py
@@ -0,0 +1,19 @@
+# https://www.acmicpc.net/problem/10102
+
+n = int(input())
+s = input()
+
+a = 0
+b = 0
+for i in s:
+    if i == 'A':
+        a += 1
+    else:
+        b += 1
+
+if a > b:
+    print('A')
+elif a < b:
+    print('B')
+else:
+    print('Tie')
