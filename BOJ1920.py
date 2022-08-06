f005f9830bb862ab85ea9ebe353221a00d960352
rigizer <68414303+rigizer@users.noreply.github.com>
2022-08-06 20:47:37 +0900

    BOJ1920 문제풀이 (이진탐색 풀이개선)

--- BOJ1920.py
+++ BOJ1920.py
@@ -1,8 +1,5 @@
 # https://www.acmicpc.net/problem/1920
 
-import sys
-sys.setrecursionlimit(100000)
-
 n = int(input())
 a = list(map(int, input().split()))
 m = int(input())
@@ -10,25 +7,26 @@ b = list(map(int, input().split()))
 
 a.sort()
 
-
-def binary_search(i, start, end):
-    if start > end:
-        return False
-
-    if a[start] > i or a[end] < i:
-        return False
-
-    mid = start + int(end / 2)
-
-    if a[mid] == i:
-        return True
-
-    if i < a[mid]:
-        return binary_search(i, start, mid - 1)
-    else:
-        return binary_search(i, mid, end)
-
-
 for i in b:
-    result = binary_search(i, 0, len(a) - 1)
+    result = True
+    start = 0
+    end = len(a) - 1
+    while True:
+        if a[start] > i or a[end] < i:
+            result = False
+            break
+
+        mid = (start + end) // 2
+
+        # 어차피 mid값 계속가면 start 혹은 end번째 인덱스에 도달
+        if i == a[mid]:
+            result = True
+            break
+
+        # mid와 일치하지 않기 때문에 -1 혹은 +1을 해줌
+        if i < a[mid]:
+            end = mid - 1
+        else:
+            start = mid + 1
+
     print(1 if result else 0)
