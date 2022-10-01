1e16d493f6be25a6798ea970e131a57aeca1b696
rigizer <rigizer@gmail.com>
2022-10-01 23:38:50 +0900

    BOJ20673 문제풀이 / Covid-19

---
+++ BOJ20673.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/20673
+
+p = int(input())
+q = int(input())
+if p <= 50 and q <= 10:
+    print("White")
+elif q > 30:
+    print("Red")
+else:
+    print("Yellow")
