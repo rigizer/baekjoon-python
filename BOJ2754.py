798f6a156435f024b5f04ad66a96d909d69ae367
rigizer <rigizer@gmail.com>
2022-10-11 23:31:45 +0900

    BOJ2754 문제풀이 / 학점계산

---
+++ BOJ2754.py
@@ -0,0 +1,17 @@
+# https://www.acmicpc.net/problem/2754
+
+point = input()
+
+if point == 'A+': print(4.3)
+elif point == 'A0': print(4.0)
+elif point == 'A-': print(3.7)
+elif point == 'B+': print(3.3)
+elif point == 'B0': print(3.0)
+elif point == 'B-': print(2.7)
+elif point == 'C+': print(2.3)
+elif point == 'C0': print(2.0)
+elif point == 'C-': print(1.7)
+elif point == 'D+': print(1.3)
+elif point == 'D0': print(1.0)
+elif point == 'D-': print(0.7)
+elif point == 'F': print(0.0)
