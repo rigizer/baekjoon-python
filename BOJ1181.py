ac6d4b94f852c487109f753f85fd79d9c7c2eb2f
rigizer <rigizer@gmail.com>
2022-10-19 10:45:24 +0900

    BOJ1181 문제풀이 / 단어 정렬

---
+++ BOJ1181.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/1181
+
+import sys
+
+n = int(sys.stdin.readline())
+lst = []
+
+for i in range(n):
+    lst.append(sys.stdin.readline().strip())
+set_lst = set(lst)
+lst = list(set_lst)
+lst.sort()
+lst.sort(key = len)
+
+for i in lst:
+    print(i)
