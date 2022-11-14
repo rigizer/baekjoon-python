5f87b6650de6fc3ac659b7dafac31ac61d23e51c
rigizer <rigizer@gmail.com>
2022-11-14 23:07:22 +0900

    BOJ2445 문제풀이 / 별 찍기 - 8

---
+++ BOJ2445.py
@@ -0,0 +1,7 @@
+# https://www.acmicpc.net/problem/2445
+
+n = int(input())
+for i in range(1, n):
+    print('*' * i + ' ' * 2 * (n - i) + '*' * i)
+for i in range(n, 0, -1):
+    print('*'  *i + ' ' * 2 * (n - i) + '*' * i)
