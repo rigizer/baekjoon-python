91f0d32af2ef64aec3df7839a13b878326583d15
rigizer <rigizer@gmail.com>
2022-11-23 19:44:17 +0900

    BOJ1010 문제풀이 / 다리 놓기

---
+++ BOJ1010.py
@@ -0,0 +1,15 @@
+# https://www.acmicpc.net/problem/1010
+
+def factorial(n):
+    num = 1
+    for i in range(1, n + 1):
+        num *= i
+    return num
+
+
+t = int(input())
+
+for _ in range(t):
+    n, m = map(int, input().split())
+    bridge = factorial(m) // (factorial(n) * factorial(m - n))
+    print(bridge)
